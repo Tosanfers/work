ebdeae1c971151a8b8431a6525676e888c085e90
Tosanfers <3010625250@qq.com>
2024-10-09 14:30:32 +0800

    last

--- search.cpp
+++ search.cpp
@@ -6,13 +6,16 @@ bool search(const vector<vector<int>> &arr, int tar);
 
 int main()
 {
-    int tar = 3;
+    int tar = 54;
     vector<vector<int>> arr = {
         {1, 3, 5, 7},
         {10, 11, 16, 20},
         {23, 30, 34, 60}};
 
+
+
     printf("%s", (search(arr, tar)) ? "true" : "false");
+    // 抽象为一个一维数组进行二分查找
 }
 
 bool search(const vector<vector<int>> &arr, int tar)
@@ -21,10 +24,10 @@ bool search(const vector<vector<int>> &arr, int tar)
     int width = arr[0].size();
     int left = 0, right = len * width - 1;
     int mid = len * width / 2;
-    int midvalue = arr[mid / len][mid % len];//获取中值
-    while (left <= right)//二分查找
+    int midvalue = arr[mid / width][mid % width]; // 获取中值
+    while (left <= right)                     // 二分查找
     {
-        midvalue = arr[mid / len][mid % len];
+        int midvalue = arr[mid / width][mid % width];
         if (midvalue < tar)
         {
             left = mid + 1;
